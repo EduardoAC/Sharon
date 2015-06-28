32245db313b123adede4682734a978e1d9c834fd
Etiene Dalcol <dalcol@etiene.net>
2015-06-28 13:36:41 +0100

    Adds icon and removes action bar

--- app/src/main/java/sharon/sharon/SharonActivity.java
+++ app/src/main/java/sharon/sharon/SharonActivity.java
@@ -91,7 +91,7 @@ public class SharonActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_sharon, menu);
+        //getMenuInflater().inflate(R.menu.menu_sharon, menu);
         return true;
     }
 
